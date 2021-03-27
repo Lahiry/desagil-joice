8ab1ecc22479d6a135187a7cdb9cc1c3f0f65e3a
rodrigogcoelhoo <rodrigogc4@al.insper.edu.br>
2021-03-27 19:55:53 -0300

    Classes CCC e CFC funcionando

--- src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -1,5 +1,22 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCCC {
+public class CalculadoraCCC extends Calculadora{
+	
+	public CalculadoraCCC() {
+		super("CCC");
+	}
 
+	@Override
+	public double calcula(double peso, double raio) {
+		
+		Double raioCm = raio * Math.pow(10, -7);
+		Double ma = 2.0 * peso;
+		Double Na = 6.023 * Math.pow(10, 23);
+		Double Vc = Math.pow((4.0 * raioCm / Math.sqrt(3)), 3);
+		
+		Double densidade = ma / (Na  * Vc);
+		
+		return densidade;
+	}
+	
 }
--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -1,5 +1,22 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCFC {
+public class CalculadoraCFC extends Calculadora {
+	
+	public CalculadoraCFC() {
+		super("CFC");
+	}
 
+	@Override
+	public double calcula(double peso, double raio) {
+		
+		Double raioCm = raio * Math.pow(10, -7);
+		Double ma = 2.0 * peso;
+		Double Na = 6.023 * Math.pow(10, 23);
+		Double Vc = 8 * Math.pow(raioCm, 3) * Math.sqrt(2);
+		
+		Double densidade = ma / (Na  * Vc);
+		
+		return densidade;
+		
+	}
 }
